190f40ff1de33fb95ecd530139d829de6e1b6ae9
mhjoy99 <75517252+MHJoy99@users.noreply.github.com>
2021-09-17 22:14:20 +0600

    Update gdriveTools.py

--- bot/helper/drive_utils/gdriveTools.py
+++ bot/helper/drive_utils/gdriveTools.py
@@ -125,9 +125,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Drive Search',
-                                 author_name='drive-searchbot',
-                                 author_url='https://github.com/breakdowns/drive-searchbot',
+                                 title = 'MHJoyBots Drive Search',
+                                 author_name='@mhjoy99',
+                                 author_url='https://github.com/mhjoy99',
                                  html_content=content)
         return
 
@@ -176,9 +176,9 @@ class GoogleDriveHelper:
 
         for content in self.telegraph_content :
             self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                    title = 'Drive Search',
-                                                    author_name='drive-searchbot',
-                                                    author_url='https://github.com/breakdowns/drive-searchbot',
+                                                    title = 'MHJoyBots Drive Search',
+                                                    author_name='@mhjoy99',
+                                                    author_url='https://github.com/mhjoy99',
                                                     html_content=content
                                                     )['path'])
 
